fca95fe155ac0e2b2d17ca4d0edc4c888d7c5bf9
ETAPD <139513318+ETAPD@users.noreply.github.com>
2025-06-01 17:38:53 +0200

    odstranenie role_ID

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('role_id');
             $table->rememberToken();
             $table->timestamps();
         });
